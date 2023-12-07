3836dc5650ee9dc05b70829a0c41057fadac35d4
noemabbbg <noemabbbg@gmail.com>
2023-12-07 19:17:42 +0300

    1

--- testN_app/models.py
+++ testN_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Comic(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+    comic = models.ForeignKey(Comic, on_delete=models.CASCADE)
+    user_id = models.IntegerField()  # предполагается, что user_id - это целое число
+    value = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+
+    def __str__(self):
+        return f"{self.comic} - {self.value}"
--- testN_app/views.py
+++ testN_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.http import JsonResponse
+from .models import Comic, Rating
+from django.views.decorators.http import require_POST
+from django.db.models import Avg
+
+@require_POST
+def create_rating(request):
+    comic_id = request.POST.get('comic_id')
+    user_id = request.POST.get('user_id')
+    value = request.POST.get('value')
+
+    comic = Comic.objects.get(id=comic_id)
+    rating, created = Rating.objects.update_or_create(
+        comic=comic, 
+        user_id=user_id, 
+        defaults={'value': value}
+    )
+
+    # Обновление рейтинга комикса
+    new_rating = Rating.objects.filter(comic=comic).aggregate(Avg('value'))['value__avg']
+    comic.rating = new_rating
+    comic.save()
+
+    return JsonResponse({'message': 'Rating updated', 'new_rating': new_rating})
+
+
+from django.views.decorators.http import require_GET
+
+@require_GET
+def get_comic_rating(request, comic_id):
+    comic = Comic.objects.get(id=comic_id)
+    return JsonResponse({'comic': comic.title, 'rating': comic.rating})
+
